1f93064efe0c3001444cddd4a8f73820381b340d
Madeline Mauser <madeline.mauser@gmail.com>
2023-03-02 11:42:49 -0700

    Add validation tests

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Subscription, type: :model do
     it { should have_many(:tea_subscriptions) }
     it { should have_many(:teas).through(:tea_subscriptions) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:frequency) }
+    it { should validate_presence_of(:user_id) }
+  end
 end
