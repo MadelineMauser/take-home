79b8d84d3ed9161f6d19ff389666c8e30190bf7e
Madeline Mauser <madeline.mauser@gmail.com>
2023-03-02 10:36:13 -0700

    Add spec for subscribe request

--- spec/requests/api/v1/subscribe_request_spec.rb
+++ spec/requests/api/v1/subscribe_request_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Subscribe", type: :request do
   before :all do
-    
+    @customer_1 = Customer.new(first_name: 'Fred', last_name: 'Williams', email: 'fredw@example.com', address: '123 Example Place')
+    @tea_1 = Tea.new(title: 'Spicy Chai', description: 'Extra spicy with cinnamon.', temperature: 212, brew_time: 4)
+    @tea_2 = Tea.new(title: 'Uncommon Chai', description: 'Uses a secret blend of spices.', temperature: 200, brew_time: 3)
   end
   describe "POST /api/v1/subscribe" do
-    it "successfully creates a customer_subscription" do
-      post '/api/v1/subscribe', params: {customer_id: , subscription_id: }
+    it "successfully creates a subscription with teas" do
+      post '/api/v1/subscribe', params: {
+        subscription: {
+          customer_id: @customer_1.id,
+          title: 'Chai Delight',
+          price: 20,
+          frequency: 3
+        }
+        tea_ids: [@tea_1.id, @tea_2.id]
+      }
       expect(response).to have_http_status(200)
     end
   end
