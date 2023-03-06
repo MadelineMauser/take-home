4020010cc771844db2bbe7b15d3bacc2f86315a5
Madeline Mauser <madeline.mauser@gmail.com>
2023-03-06 09:52:26 -0700

    Add coverage to gitignore

--- app/controllers/api/v1/customers/subscriptions_controller.rb
+++ app/controllers/api/v1/customers/subscriptions_controller.rb
@@ -1,2 +1,14 @@
 class SubscriptionsController < ApplicationController
+  def create
+    new_subscription = Subscription.new(subscription_params, customer_id: params[:id])
+    if new_subscription.save
+      
+    end
+  end
+end
+
+private 
+
+def subscription_params
+  params.require(:subscription).permit(:title, :price, :status, :frequency)
 end
---
+++ app/facades/subscriptions_facade.rb
@@ -0,0 +1,5 @@
+class SubscriptionsFacade
+  def self.add_teas(tea_array)
+    
+  end
+end
